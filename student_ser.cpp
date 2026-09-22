0225d62f9985df1d150bc45ab49b6a128df881f3
sanchitpisal08 <sanchitpisal1@gmail.com>
2026-09-22 15:05:32 +0530

    Create student_ser.cpp

---
+++ student_ser.cpp
@@ -0,0 +1,26 @@
+include<iostream>
+using namespace std;
+
+int main()
+{
+int student[5];
+int searchID;
+
+cout<<"enter roll_no of 5 student:";
+for(int i=0;i<5;i++)
+{
+cin>>student[i];
+}
+cout<<"enter roll no of 5 student:";
+cin>>searchID;
+for(int i=0; i<5;i++)
+{
+if(student[i]==searchID)
+{
+cout<<"student found!";
+return 0;
+}
+}
+cout<<"student not found";
+return 0;
+}
